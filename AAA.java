5a8472eeee25647d94e40dd327145c9e52e92697
maciagr <grzegorz.maciag@ext.ec.europa.eu>
2022-01-17 14:29:54 +0100

    change 2

--- AAA.java
+++ AAA.java
@@ -1,5 +1,8 @@
 public class AAA {
+
     public static void main(String[] argv) {
+        Gen gen = new Gen();
         System.out.println("AAAA");
+        System.out.println(gen.toString());
     }
 }
---
+++ Gen.java
@@ -0,0 +1,6 @@
+public class Gen {
+    @Override
+    public String toString() {
+        return "Gen{}";
+    }
+}
